64d0fe36f82e8ba7ecb03e000d5368e622156d89
Jocelyn Peña <jocelyn.p.reyes332b@gmail.com>
2023-11-10 13:30:44 -0600

    Se agrega ejercicio cambio de nombre

---
+++ script.js
@@ -0,0 +1,9 @@
+function changeName() {
+    const name = prompt("¿Cuál es tu nombre?");
+    const refName = getReferenceName();
+    refName.innerHTML = name;
+}
+
+function getReferenceName(){
+    return document.getElementById("name");
+}
